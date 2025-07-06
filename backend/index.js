97cf2c8613b265436de664a31aa11743f0570a54
khushmnsangha <anmolsangha02@gmail.com>
2025-07-06 22:23:15 +0530

    Serve React build in backend and fixed module syntax

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(cors());
 app.use("/", routes);
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "..", "frontend", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
